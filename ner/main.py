aa396a5e76b79b437e33188159db58dc89ecb250
Zemsky Max <zemskymax@gmail.com>
2024-09-21 23:01:44 +0300

    Add syntetic data generation implementation #2 (In Progress)

---
+++ ner/main.py
@@ -0,0 +1,136 @@
+import json
+import re
+import random
+from vllm import LLM, SamplingParams
+from names import *
+
+# LLM_MODEL = "mistralai/Mistral-7B-Instruct-v0.3"
+# LLM_MODEL = "solidrust/Mistral-7B-Instruct-v0.3-AWQ"
+LLM_MODEL = "neuralmagic/Mistral-7B-Instruct-v0.3-GPTQ-4bit"
+NUM_GPUs = 1
+
+
+def create_json_prompt_for_synthetic_data(**kwargs):
+    # Building the initial part of the prompt
+    prompt = """
+        **Objective:**
+        Generate realistic text passages that include named entities. Each entity should be clearly identified and labeled with its type(s) for easy extraction.
+
+        **Format Requirements:**
+        - Output should be formatted in JSON and include both the text and a list of entities.
+        - No additional comments or explanations should be included in the output.
+        - Each entity must be accurately labeled and appear in the entities list.
+        - Follow all attribute requirements exactly as specified.
+        - Output only the JSON object.
+
+        **Entity Annotation Details:**
+        - All entity types must be in lowercase. For example, use "type" instead of "TYPE".
+        - Entity types can contain multiple words, separated by spaces (e.g., "entity type", not "entity_type").
+        - Nested entities are allowed (an entity can be within another entity).
+        - An entity can be associated with multiple types. In such cases, list them under the "types" key.
+
+        **Output Schema Example:**
+        <start attribute_1="value1" attribute_2="value2" ...>
+        {
+        "text": "text content",
+        "entities": [
+            {"entity": "entity name", "types": ["type 1", "type 2", ...]},
+            ...
+        ]
+        }
+        <end>
+
+        **Finish the following Schema:**"""
+
+    # Use dictionary comprehension to filter out 'n/a' values and to keep the code flexible
+    attributes = {key: value for key, value in kwargs.items() if value != "n/a"}
+
+    # Create a string of attributes for the <start> tag, excluding any 'n/a' values
+    attributes_string = " ".join([f'{key}="{value}"' for key, value in attributes.items()])
+
+    # Adding the dynamically created attributes string to the prompt
+    prompt += f"""
+        <start {attributes_string}>"""
+
+    return prompt
+
+##----------------------------------------##
+
+def tokenize_text(text):
+    """Tokenize the input text into a list of tokens."""
+    return re.findall(r'\w+(?:[-_]\w+)*|\S', text)
+
+##----------------------------------------##
+
+def extract_entities(data):
+    all_examples = []
+
+    for dt in data:
+        # Attempt to extract entities; skip current record on failure
+        try:
+            tokens = tokenize_text(dt['text'])
+            ents = [(k["entity"], k["types"]) for k in dt['entities']]
+        except Exception as ex:
+            print(f"Exception: {ex}!")
+            continue
+
+        spans = []
+        for entity in ents:
+            entity_tokens = tokenize_text(str(entity[0]))
+
+            # Find the start and end indices of each entity in the tokenized text
+            for i in range(len(tokens) - len(entity_tokens) + 1):
+                if " ".join(tokens[i:i + len(entity_tokens)]).lower() == " ".join(entity_tokens).lower():
+                    for el in entity[1]:
+                        spans.append((i, i + len(entity_tokens) - 1, el.lower().replace('_', ' ')))
+
+        # Append the tokenized text and its corresponding named entity recognition data
+        all_examples.append({"tokenized_text": tokens, "ner": spans})
+
+    return all_examples
+
+##----------------------------------------##
+
+def generate_from_prompts(prompt, llm, sampling_params):
+    outputs = llm.generate(prompt, sampling_params)
+    # print(f"> total outputs:\n {outputs}")
+
+    all_outs = []
+    for output in outputs:
+        try:
+            # print(f"-> raw output:\n {output.outputs[0].text.strip()}")
+            js = json.loads(output.outputs[0].text.strip())
+        except Exception as ex:
+            print(f"Exception: {ex}!")
+            continue
+
+        # print(f"--> output as JSON:\n {js})")
+        all_outs.append(js)
+
+    return all_outs, extract_entities(all_outs)
+
+
+NUM_SAMPLES = 3
+if __name__ == "__main__":
+    llm = LLM(model=LLM_MODEL, gpu_memory_utilization=0.8, max_model_len=1500, tensor_parallel_size=NUM_GPUs, seed=17, quantization="GPTQ")
+
+    sampling_params = SamplingParams(top_k=100, max_tokens=1000, top_p=0.8, stop="<end>")
+
+    all_outputs = []
+    for i in range(NUM_SAMPLES):
+        name = random.choice(FIRST_NAMES)
+        surname = random.choice(SECOND_NAMES)
+
+        prompt = create_json_prompt_for_synthetic_data(language="english",
+                                                       types_of_text="casual sentence",
+                                                       name=name,
+                                                       surname=surname)
+
+        output, processed_output = generate_from_prompts(prompt, llm, sampling_params)
+        all_outputs.append(output)
+
+        print(f"Final response: {output}")
+        print(f"Processed output {processed_output}")
+
+    # TODO. save to file
+    print("Done")
---
+++ ner/names.py
@@ -0,0 +1,81 @@
+import random
+
+COUNTRIES = [
+    "Madagascar", "Taiwan", "USA", "Germany", "France", "Spain", "Russia", "China", 
+    "Japan", "Brazil", "India", "Egypt", "South Africa", "Australia", "Canada", 
+    "Mexico", "Indonesia", "Nigeria", "Turkey", "United Kingdom", "Italy", "Poland", 
+    "Argentina", "Netherlands", "Belgium", "Switzerland", "Sweden", "Norway", "Finland",
+    "Denmark", "Portugal", "Greece", "Iran", "Thailand", "Philippines", "Vietnam", 
+    "South Korea", "Saudi Arabia", "Israel", "UAE", "New Zealand", "Ireland", "Malaysia",
+    "Singapore", "Hong Kong", "Czech Republic", "Hungary", "Romania", "Colombia", 
+    "Peru", "Venezuela", "Chile", "Morocco", "Algeria", "Tunisia", "Nepal", "Pakistan", "Bangladesh", 
+    "Kazakhstan", "Ukraine", "Austria", "Croatia", "Serbia", "Kenya", "Ghana", "Zimbabwe",
+    "Cuba", "Panama", "Fiji", "Mongolia", "North Korea", "Myanmar", "Ethiopia", "Tanzania",
+    "Algeria", "Libya", "Jordan", "Qatar", "Oman", "Kuwait", "Lebanon", "Bulgaria", "Slovakia",
+    "Lithuania", "Latvia", "Estonia", "Cyprus", "Luxembourg", "Macao", "Bhutan", "Maldives",
+    "Angola", "Cameroon", "Senegal", "Mali", "Zambia", "Uganda", "Namibia", "Botswana",
+    "Mozambique", "Ivory Coast", "Burkina Faso", "Malawi", "Gabon", "Lesotho", "Gambia",
+    "Guinea", "Cape Verde", "Rwanda", "Benin", "Burundi", "Somalia", "Eritrea", "Djibouti",
+    "Togo", "Seychelles", "Chad", "Central African Republic", "Liberia", "Mauritania", "Sri Lanka",
+    "Sierra Leone", "Equatorial Guinea", "Swaziland", "Congo (Kinshasa)", "Congo (Brazzaville)"
+]
+
+FIRST_NAMES = [
+    "James", "John", "Robert", "Michael", "William", "Matteo", "Richard", "Joseph", "Charles", "Lorenzo", 
+    "Christopher", "Aurora", "Matthew", "Anthony", "Donald", "Paul", "Mark", "Steven", "Andrew", "Kenneth", 
+    "George", "Brian", "Edward", "Ronald", "Timothy", "Jason", "Jeffrey", "Ryan", "Frank", "Kevin", "Ivan", 
+    "Alexander", "Dmitry", "Sergey", "Mikhail", "Vladimir", "Andrei", "Nikolai", "Igor", "Pavel", "Sophia", 
+    "Marcos", "Maria", "Hannah", "Ana", "Lucas", "Leon", "Pedro", "Noah", "Emma", "Xu", "Sun", "Ma", "Zhu", 
+    "Lin", "Guo", "Cheng", "He", "Luo", "Cao", "María", "José", "Juan", "Carlos", "Francisco", "Antonio", 
+    "Manuel", "Alejandro", "Diego", "Miguel", "David", "Javier", "Felix", "Rafael", "Fernando", "Luis", 
+    "Daniel", "Beatrice", "Eduardo", "Alberto", "Roberto", "Pablo", "Sergio", "Jorge", "Oscar", "Ricardo", 
+    "Ángel", "Santiago", "Emilio", "Domingo", "Andrés", "Muhammad", "Ahmed", "Ali", "Omar", "Hassan", 
+    "Ibrahim", "Max", "Karim", "Hussein", "Mahmoud" ]
+
+SECOND_NAMES = [
+    "Sato", "Suzuki", "Takahashi", "Tanaka", "Watanabe", "Ito", "Yamamoto", "Nakamura", "Kobayashi", 
+    "Kato", "Kim", "Lee", "Park", "Choi", "Jung", "Kang", "Yoo", "Jang", "Ahn", "Song", "Smith", 
+    "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Rodriguez", "Martinez", 
+    "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", 
+    "White", "Papadopoulos", "Giannakopoulos", "Georgiou", "Dimitriou", "Nikolaidis", "Angelopoulos", 
+    "Petrou", "Ioannidis", "Christodoulou", "Konstantinou", "Vlachos", "Karagiannis", "Alexandrou", 
+    "Kouros", "Manolakis", "Tsiamis", "Zacharias", "Tsakiris", "Panagiotopoulos", "Stefanidis", 
+    "Ivanov", "Smirnov", "Kuznetsov", "Popov", "Sokolov", "Vasiliev", "Petrov", "Sidorov", "Mikhailov", 
+    "Nikolaev", "Wang", "Li", "Zhang", "Liu", "Chen", "Yang", "Huang", "Zhao", "Wu", "Zhou", "Kenyatta", 
+    "Otieno", "Mwangi", "Njoroge", "Kiplagat", "Ndlovu", "Zulu", "Dlamini", "Mthembu", "Sithole", "Diop", 
+    "Faye", "Mohamed", "Farouk", "Amin", "Youssef", "Salah", "Gaber", "Shawky", "Tawfik", "El Sayed" ]
+
+def find_duplicates(arr):
+    return set([x for x in arr if arr.count(x) > 1])
+
+def find_common_elements(arr1, arr2):
+    # Convert arrays to sets for faster lookup
+    set1 = set(arr1)
+    set2 = set(arr2)
+    # Find intersection of the sets (common elements)
+    common_elements = set1.intersection(set2)
+    return list(common_elements)
+
+
+if __name__ == "__main__":
+    print("--data statistics--")
+
+    names_duplicates = find_duplicates(FIRST_NAMES)
+    print(f"There are {len(names_duplicates)} duplicates in names: {names_duplicates}")
+
+    surnames_duplicates = find_duplicates(SECOND_NAMES)
+    print(f"There are {len(surnames_duplicates)} duplicates in surnames: {surnames_duplicates}")
+
+    common_elements = find_common_elements(FIRST_NAMES, SECOND_NAMES)
+    print(f"There are {len(common_elements)} common elements: {common_elements}")
+
+    print(f"There are {len(FIRST_NAMES)} names, and {len(SECOND_NAMES)} surnames")
+    assert len(FIRST_NAMES) == len(SECOND_NAMES)
+
+    print("--data examples--")
+    random_samples = 5
+    for i in range(random_samples):
+        name = random.choice(FIRST_NAMES)
+        surname = random.choice(SECOND_NAMES)
+
+        print(f"{i+1}: {name} {surname}")
